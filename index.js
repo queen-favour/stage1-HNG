eae42e74cb6b572b8c89c138e4f0a602bbea5239
queen-favour <elechifavour3@gmail.com>
2023-09-08 02:01:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//JS FOR DYNAMIC DATA
+
+function updateTimeAndDay() {
+    const Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDate = new Date();
+
+    const currentDay = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    currentDay.textContent = Days[currentDate.getDay()];
+
+    const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
+    currentUTCTime.textContent = currentDate.toUTCString();
+}
+
+updateTimeAndDay();
+
+setInterval(updateTimeAndDay, 1000);
